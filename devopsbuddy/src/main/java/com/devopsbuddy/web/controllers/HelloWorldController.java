f3f5d9c896dc56b61602f56d422c35674f9f7a5f
WazaK <wazakaunda@gmail.com>
2017-05-28 23:47:27 +0200

    architecture setup

--- devopsbuddy/src/main/java/com/devopsbuddy/web/controllers/HelloWorldController.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/web/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.devopsbuddy.controllers;
+package com.devopsbuddy.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
