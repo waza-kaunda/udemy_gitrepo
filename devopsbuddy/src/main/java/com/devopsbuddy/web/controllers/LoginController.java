29b54b87d46cde95c14223572dfc7d1faa49d5c5
WazaK <wazakaunda@gmail.com>
2017-06-13 09:55:04 +0200

    added login page and login controller. updated navbar and messages properties file

---
+++ devopsbuddy/src/main/java/com/devopsbuddy/web/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package com.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	private static final String LOGIN_VIEW_NAME = "user/login";
+	
+	@RequestMapping("/login")
+	public String login() {
+		return LOGIN_VIEW_NAME;
+	} 
+}
