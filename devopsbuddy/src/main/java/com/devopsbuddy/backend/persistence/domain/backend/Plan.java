be79bb7a4b1a396e6490a2f86aec7603a6dfdb32
WazaK <wazakaunda@gmail.com>
2017-06-14 12:29:37 +0200

    Squashed commit of the following:
    commit 5f4acd58f9a30691c0812ee44a461aa7797f6b21
    Author: WazaK <wazakaunda@gmail.com>
    Date:   Wed Jun 14 12:28:51 2017 +0200
    
    	Created JPA Entity models for Spring security

---
+++ devopsbuddy/src/main/java/com/devopsbuddy/backend/persistence/domain/backend/Plan.java
@@ -0,0 +1,62 @@
+package com.devopsbuddy.backend.persistence.domain.backend;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Plan implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+	
+	private String name;
+	
+	/** Default constructor. */
+	public Plan() {
+	
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Plan other = (Plan) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+}
