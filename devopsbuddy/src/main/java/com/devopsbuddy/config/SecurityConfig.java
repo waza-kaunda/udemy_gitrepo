57a5cbdf2e47ec2c15bf4a61ba428461b063ae70
WazaK <wazakaunda@gmail.com>
2017-06-17 23:53:29 +0200

    Added password encryption with BCrypt.

--- devopsbuddy/src/main/java/com/devopsbuddy/config/SecurityConfig.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -1,44 +1,53 @@
 package com.devopsbuddy.config;
 
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.devopsbuddy.backend.service.UserSecurityService;
-import com.devopsbuddy.backend.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	/** The encryption SALT */
+	private static final String SALT = "a;jo941[p4./\'adlkasp[3400g-wi4";
+
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private UserSecurityService userSecurityService;
-	
+
 	/** Public URLs */
 	private static final String[] PUBLIC_MATCHERS = { "/webjars/**", "/css/**", "/js/**", "/images/**", "/",
 			"/about/**", "/contact/**", "/error/**/*", "/console/**" };
 
-	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		List<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
-		
-		if(activeProfiles.contains("dev")){
+
+		if (activeProfiles.contains("dev")) {
 			http.csrf().disable();
 			http.headers().frameOptions().disable();
 		}
-		
+
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated().and().formLogin()
 				.loginPage("/login").defaultSuccessUrl("/payload").failureUrl("/login?error").permitAll().and().logout()
 				.permitAll();
@@ -46,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userSecurityService);
+		auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
 	}
 
 }
