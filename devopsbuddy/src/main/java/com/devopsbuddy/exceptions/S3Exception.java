fae29eddf184b0a94eff29158b7a4472dfe8da33
WazaK <wazakaunda@gmail.com>
2017-07-03 22:14:53 +0200

    Squashed commit of the following:
    commit 049fb1aa86adb987e9f9ed5db206b875d51f0b1c
    Author: WazaK <wazakaunda@gmail.com>
    Date:   Mon Jul 03 22:12:36 2017 +0200
    
    	Added exception handling for Strpie service and Amazon web service.

---
+++ devopsbuddy/src/main/java/com/devopsbuddy/exceptions/S3Exception.java
@@ -0,0 +1,13 @@
+package com.devopsbuddy.exceptions;
+
+public class S3Exception extends RuntimeException {
+
+	public S3Exception(Throwable e) {
+		super(e);
+	}
+
+	public S3Exception(String s) {
+		super(s);
+	}
+
+}
